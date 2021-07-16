69039fb84cdf5130b74bd2d4ae7508df16413f39
Jake <jake.eilbeck@gmail.com>
2021-07-16 21:28:05 +0100

    Minor tweaks

--- app/src/main/java/com/example/composecocktails/ui/theme/Color.kt
+++ app/src/main/java/com/example/composecocktails/ui/theme/Color.kt
@@ -3,16 +3,16 @@ package com.example.composecocktails.ui.theme
 import androidx.compose.ui.graphics.Color
 
 //Light colours
-val Pink900 = Color(0xFF880e4f)//primary
-val Pink700 = Color(0xFFc2185b)//primaryVariant
-val Red = Color(0xFF880e12)//secondary
+val Pink900 = Color(0xFF880e4f)     //primary
+val Pink700 = Color(0xFFc2185b)     //primaryVariant
+val Red = Color(0xFF880e12)         //secondary
 val Pink900Light = Color(0xFFbc477b)//background
-val Pink900Dark = Color(0xFF560027)//surface
+val Pink900Dark = Color(0xFF560027) //surface
 
 //Dark colours
 val DarkBlueGray = Color(0xFF01152f)//primary
-val Charcoal = Color(0xFF2C3B58)//primaryVariant
+val Charcoal = Color(0xFF2C3B58)    //primaryVariant
 val CharcoalDark = Color(0xFF364156)//secondary
-val DarkGreyBg = Color(0xFF121212)//background
+val DarkGreyBg = Color(0xFF121212)  //background
 val PrussianBlue = Color(0xFF11151C)//surface
-val error = Color(0xFF7f0000)//error
+val error = Color(0xFF7f0000)       //error
