0b80c7fa283efc7e83d25a87233255a83265d5a1
Jake <jake.eilbeck@gmail.com>
2021-11-10 19:54:47 +0000

    Updated connectivity checker due to bugs in previous implementation

--- app/src/main/java/com/example/composecocktails/util/Utils.kt
+++ app/src/main/java/com/example/composecocktails/util/Utils.kt
@@ -2,26 +2,15 @@ package com.example.composecocktails.util
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities
 
 class Utils(private val context: Context) {
 
-    //https://stackoverflow.com/a/65239349
     fun isOnlineCheck(): Boolean {
 
-        val connectivityMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        //https://stackoverflow.com/a/68643147
+        val connectivityManager = context.getSystemService(ConnectivityManager::class.java) as ConnectivityManager
+        val currentNetwork = connectivityManager.activeNetwork
 
-        val allNetworks: Array<Network> = connectivityMgr.allNetworks
-
-        for (network in allNetworks) {
-            val networkCapabilities = connectivityMgr.getNetworkCapabilities(network)
-            return (networkCapabilities!!.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
-                    networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) &&
-                    (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
-                            || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
-                            || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)))
-        }
-        return false
+        return currentNetwork != null
     }
 }
